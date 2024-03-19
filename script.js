3cdab095ad655f23789ac3c691beae5b6c6978d9
Bowie <vinihenriquesarti@gmail.com>
2024-03-18 22:31:02 -0300

    index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+function alterarTexto(tag, texto){
+  let campo = document.querySelector(tag);
+  campo.innerHTML=texto;
+}
+
+function saida(){
+  let saida = document.querySelector('.codificador__conteudo__input');
+}
